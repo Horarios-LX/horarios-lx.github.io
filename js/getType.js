dad8933b20f63605c437e705af731098fd0f8bc7
Movies22 <111647781+Movs22@users.noreply.github.com>
2024-10-25 15:42:16 +0100

    Changed getType.js
    Minor change to fix a misclassification of certain buses

--- js/getType.js
+++ js/getType.js
@@ -3,8 +3,8 @@ function getVehicle(vehicleId) {
     let v = vehicleId.split("|")[1]
     v = parseInt(v)
     if (v === 1901) return "Autocarro com 3 portas"
-    if (v < 730 || v > 739 && v < 750 || v > 1799) return "Autocarro com 2 portas"
-    if ((v > 729 && v < 740) || (v > 799 && v < 900) || (v > 1699 && v < 1800)) return "Carrinha"
+    if (v < 730 || v > 1799) return "Autocarro com 2 portas"
+    if ((v > 729 && v < 750) || (v > 799 && v < 900) || (v > 1699 && v < 1800)) return "Carrinha"
     if (v > 749 && v < 1500) return "Autocarro com 3 portas"
     return "UNKNOWN"
 }
