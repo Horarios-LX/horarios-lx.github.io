7e409091660be20e07eb1622dd2d532c45536daa
Movies22 <111647781+Movs22@users.noreply.github.com>
2024-12-13 20:18:15 +0000

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-let CLOUDFLARED = "https://pst-guitar-influences-compatibility.trycloudflare.com/"
+let CLOUDFLARED = "https://henry-wrestling-belly-ownership.trycloudflare.com/"
 
 const API_BASE = "https://api.cmet.pt/";
 
